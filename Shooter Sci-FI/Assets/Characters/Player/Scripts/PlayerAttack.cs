8e740c9147bd483633a7da624b0260a32a8b2ee5
MikhailSumarokov <ykpon1995@mail.ru>
2023-01-14 11:48:59 +0500

    0.36
    Debug PlayerAttack

--- Shooter Sci-FI/Assets/Characters/Player/Scripts/PlayerAttack.cs
+++ Shooter Sci-FI/Assets/Characters/Player/Scripts/PlayerAttack.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private Weapon _gun;
+    private Weapon[] _weapons;
 
     private void Start()
     {
-        _gun = transform.GetComponentInChildren<Weapon>();
+        _weapons = transform.GetComponentsInChildren<Weapon>();
     }
 
     private void Update()
@@ -22,6 +22,15 @@ public class PlayerAttack : MonoBehaviour
         Vector3 direction;
         if (hit.collider != null) direction = hit.point;
         else direction = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 50f));
-        _gun.Fire(direction);
+        SearchCurrentWeapon()?.Fire(direction);
+    }
+
+    private Weapon SearchCurrentWeapon()
+    {
+        Weapon currentWeapon = null;
+        foreach (var weapon in _weapons)
+            if (weapon.gameObject.activeInHierarchy)
+                currentWeapon = weapon;
+        return currentWeapon;
     }
 }
